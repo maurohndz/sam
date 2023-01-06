4446a65715af5f29ac32ef809dca220b56627a16
Mauro Hernandez <mauroDeveloper@hotmail.com>
2023-01-06 12:50:42 -0400

    ok

--- js/tabs.js
+++ js/tabs.js
@@ -58,38 +58,19 @@ function tabMainControl() {
 	const ACTIVE = "mt_active";
 	let tabs = $(".mt_menu");
 
-	if (tabs.is(":visible")) {
-		let active_id = $(`.mt_content.${ACTIVE}`).get(0).id;
-		let link_active = $(`.mt_menu_link[href$='${active_id}']`);
+	$(".mt_menu_link").on("click", (event) => {
+		event.preventDefault();
 
+		$(".mt_content").removeClass(ACTIVE)
 		$(".mt_menu_link").removeClass(ACTIVE)
-        $(link_active).addClass(ACTIVE)
 
-		tabs.find("a").on("click", function (event) {
-			event.preventDefault();
+		let id = $(event.currentTarget).attr("href")
 
-			let target = $(this).attr("href")
+		$(`.mt_menu_link[href$='${id}']`).addClass(ACTIVE);
+		$(id).addClass(ACTIVE);
+	})
 
-			$(".mt_menu_link").removeClass(ACTIVE)
-			$(".mt_content").removeClass(ACTIVE)
-
-			$(`.mt_menu_link[href$='${target}']`).addClass(ACTIVE);
-			$(target).addClass(ACTIVE);
-		})
-	} else {
-		$(".mt_menu_link").on("click", (event) => {
-			event.preventDefault();
-
-			$(".mt_content").removeClass(ACTIVE)
-			$(".mt_menu_link").removeClass(ACTIVE)
-
-			let target = event.currentTarget
-			let content_id = $(target).attr("href")
-
-			$(target).addClass(ACTIVE);
-			$(content_id).addClass(ACTIVE);
-		})
-
+	if (!tabs.is(":visible")) {
 		$(".mt_content").on("click", ({ currentTarget }) => {
 			let id = currentTarget.id;
 			let links = $(`.mt_menu_link[href$='${id}']`);
