038960bfabab0c91e4dd2f2a175c867934f82bfb
Mauro Hernandez <mauroDeveloper@hotmail.com>
2023-01-06 12:57:57 -0400

    ok 2

--- js/tabs.js
+++ js/tabs.js
@@ -15,41 +15,28 @@ function tabMountainControl() {
 	let tabs = $(".tabbed_content").find(".tabs_menu");
 
 	if (tabs.is(":visible")) {
-        let tabs_item_active = tabs.parents(".tabbed_content").find(`.tabs_item.${ACTIVE}`).get(0).id;
-        let tab_active =  $(`.tabs_menu_link[href$='${tabs_item_active}']`).get(0);
-
-        $(".tabs_menu_link").removeClass(ACTIVE)
-        $(tab_active).addClass(ACTIVE)
-
-		tabs.find("a").on("click", function (event) {
+		$(".tabs_menu_link").on("click", (event) => {
 			event.preventDefault();
-
-			let target = $(this).attr("href"),
-				tabs = $(this).parents(".tabs_menu"),
-				buttons = tabs.find(".tabs_menu_link"),
-				item = tabs.parents(".tabbed_content").find(".tabs_item");
-
-			buttons.removeClass(ACTIVE);
-			item.removeClass(ACTIVE);
-
-			$(this).addClass(ACTIVE);
-			$(target).addClass(ACTIVE);
-		});
+	
+			$(".tabs_item").removeClass(ACTIVE)
+			$(".tabs_menu_link").removeClass(ACTIVE)
+	
+			let id = $(event.currentTarget).attr("href")
+	
+			$(`.tabs_menu_link[href$='${id}']`).addClass(ACTIVE);
+			$(id).addClass(ACTIVE);
+		})
 	} else {
-		$(".tabs_item").on("click", function () {
-			let container = $(this).parents(".tabbed_content"),
-				currId = $(this).attr("id"),
-				items = container.find(".tabs_item");
+		$(".tabs_item").on("click", ({ currentTarget }) => {
+			let id = currentTarget.id;
+			let links = $(`.tabs_menu_link[href$='${id}']`);
 
-			container.find("tabs_menu_link").removeClass(ACTIVE);
-			items.removeClass(ACTIVE);
+			$(".tabs_item").removeClass(ACTIVE)
+			$(".tabs_menu_link").removeClass(ACTIVE)
 
-			$(this).addClass(ACTIVE);
-			
-			container
-				.find('.tabs a[href$="#' + currId + '"]')
-				.addClass(ACTIVE);
-		});
+			$(currentTarget).addClass(ACTIVE);
+			$(links).addClass(ACTIVE);
+		})
 	}
 }
 
